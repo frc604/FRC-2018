464ef2e2b0fa69f71078458ba960e60990002228
rlee287 <rlee287@yahoo.com>
2018-01-06 09:56:57 -0800

    Update TalonPWMEncoder for new CTRE drivers

--- src/com/_604robotics/robotnik/prefabs/devices/TalonPWMEncoder.java
+++ src/com/_604robotics/robotnik/prefabs/devices/TalonPWMEncoder.java
@@ -1,50 +1,51 @@
 package com._604robotics.robotnik.prefabs.devices;
 
-import com.ctre.CANTalon;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 
 public class TalonPWMEncoder implements PIDSource {
-	private final CANTalon talon;
-	private PIDSourceType sourceType;
+    private final TalonSRX talon;
+    private PIDSourceType sourceType;
 
-	public TalonPWMEncoder (CANTalon talon) {
-		this(talon, PIDSourceType.kDisplacement);
-	}
+    public TalonPWMEncoder (TalonSRX talon) {
+        this(talon, PIDSourceType.kDisplacement);
+    }
 
-	public TalonPWMEncoder (CANTalon talon, PIDSourceType sourceType) {
-		this.talon = talon;
-		this.sourceType = sourceType;
-	}
-	
-	public double getPosition () {
-		return talon.getPulseWidthPosition();
-	}
-	
-	public double getVelocity () {
-		return talon.getPulseWidthVelocity();
-	}
+    public TalonPWMEncoder (TalonSRX talon, PIDSourceType sourceType) {
+        this.talon = talon;
+        this.sourceType = sourceType;
+    }
 
-	@Override
-	public double pidGet () {
-		if (sourceType.equals(PIDSourceType.kDisplacement)) {
-			return getPosition();
-		} else if (sourceType.equals(PIDSourceType.kRate)) {
-			return getVelocity();
-		} else {
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	@Override
-	public PIDSourceType getPIDSourceType () {
-		return sourceType;
-	}
+    // Verify getPulseWidth vs getQuadrature
+    public double getPosition () {
+        return talon.getSensorCollection().getPulseWidthPosition();
+    }
 
-	@Override
-	public void setPIDSourceType (PIDSourceType sourceType) {
-		this.sourceType = sourceType;
-	}
+    public double getVelocity () {
+        return talon.getSensorCollection().getPulseWidthVelocity();
+    }
+
+    @Override
+    public double pidGet () {
+        if (sourceType.equals(PIDSourceType.kDisplacement)) {
+            return getPosition();
+        } else if (sourceType.equals(PIDSourceType.kRate)) {
+            return getVelocity();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public PIDSourceType getPIDSourceType () {
+        return sourceType;
+    }
+
+    @Override
+    public void setPIDSourceType (PIDSourceType sourceType) {
+        this.sourceType = sourceType;
+    }
 }
 
 
