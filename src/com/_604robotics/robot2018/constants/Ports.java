d8402a3dd2f05ae2f4c8b4b9a1dca12736884022
Ryan Lee <rlee287@yahoo.com>
2018-01-11 20:11:38 -0800

    Add third motor to Ports and change ports to actually reflect Frank

--- src/com/_604robotics/robot2018/constants/Ports.java
+++ src/com/_604robotics/robot2018/constants/Ports.java
@@ -6,23 +6,28 @@ public class Ports {
     private Ports () {}
     
     // Motors
+    // Remember to take note of PDP ports for these too once they are determined
     @Unreal("Wiring yet to be determined. Use Frank wiring for now")
-    public static final int DRIVE_FRONT_LEFT_MOTOR = 7;
+    public static final int DRIVE_FRONT_LEFT_MOTOR = 0;
     @Unreal("Wiring yet to be determined. Use Frank wiring for now")
-    public static final int DRIVE_REAR_LEFT_MOTOR = 5;
+    public static final int DRIVE_REAR_LEFT_MOTOR = 1;
     @Unreal("Wiring yet to be determined. Use Frank wiring for now")
-    public static final int DRIVE_FRONT_RIGHT_MOTOR = 0;
+    public static final int DRIVE_FRONT_RIGHT_MOTOR = 2;
     @Unreal("Wiring yet to be determined. Use Frank wiring for now")
-    public static final int DRIVE_REAR_RIGHT_MOTOR = 9;
+    public static final int DRIVE_REAR_RIGHT_MOTOR = 3;
+    @Unreal("Third motor for drive yet to be wired")
+    public static final int DRIVE_TOP_LEFT_MOTOR = 4;
+    @Unreal("Third motor for drive yet to be wired")
+    public static final int DRIVE_TOP_RIGHT_MOTOR = 5;
 
     // Digital Inputs
-    @Unreal("Wiring yet to be determined. Use Frank wiring for now")
+    @Unreal("Wiring yet to be determined")
     public static final int ENCODER_LEFT_A = 2;
-    @Unreal("Wiring yet to be determined. Use Frank wiring for now")
+    @Unreal("Wiring yet to be determined")
     public static final int ENCODER_LEFT_B = 3;
-    @Unreal("Wiring yet to be determined. Use Frank wiring for now")
+    @Unreal("Wiring yet to be determined")
     public static final int ENCODER_RIGHT_A = 0;
-    @Unreal("Wiring yet to be determined. Use Frank wiring for now")
+    @Unreal("Wiring yet to be determined")
     public static final int ENCODER_RIGHT_B = 1;
    
     // Analog
@@ -34,6 +39,6 @@ public class Ports {
     // CAN
     @Unreal("Wiring yet to be determined. Use Frank wiring for now."
           + "These will probably stay the same, but just in case...")
-    public static final int COMPRESSOR = 0;
-    public static final int PDP_MODULE = 1;
+    public static final int COMPRESSOR = 1;
+    public static final int PDP_MODULE = 0;
 }
