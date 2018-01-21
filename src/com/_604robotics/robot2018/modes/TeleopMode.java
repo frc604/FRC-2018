c7aaf9a603aa49a3f2663e33b0078cae4f9465c5
squeakadoodle <squeakadoodle6084@gmail.com>
2018-01-21 10:54:09 -0800

    Added an elevator that currently only has manual control and some neat holding stuff, as well as a detector for when its broken with a fallback

--- src/com/_604robotics/robot2018/modes/TeleopMode.java
+++ src/com/_604robotics/robot2018/modes/TeleopMode.java
@@ -3,13 +3,12 @@ package com._604robotics.robot2018.modes;
 import com._604robotics.robot2018.Robot2018;
 import com._604robotics.robot2018.constants.Calibration;
 import com._604robotics.robot2018.modules.Drive;
+import com._604robotics.robot2018.modules.Elevator;
 import com._604robotics.robotnik.Coordinator;
-import com._604robotics.robotnik.prefabs.flow.SmartTimer;
 import com._604robotics.robotnik.prefabs.flow.Toggle;
 import com._604robotics.robotnik.prefabs.inputcontroller.xbox.XboxController;
 
 public class TeleopMode extends Coordinator {
-    private boolean started = false;
 
     private final XboxController driver = new XboxController(0);
     private final XboxController manip = new XboxController(1);
@@ -17,6 +16,7 @@ public class TeleopMode extends Coordinator {
     private final Robot2018 robot;
 
     private final DriveManager driveManager;
+    private final ElevatorManager elevatorManager;
 
 
 
@@ -48,14 +48,36 @@ public class TeleopMode extends Coordinator {
         this.robot = robot;
 
         driveManager = new DriveManager();
+        elevatorManager = new ElevatorManager();
     }
 
     @Override
     public boolean run () {
         driveManager.run();
+        elevatorManager.run();
         return true;
-    }    
-
+    }
+    
+    private class ElevatorManager {
+    	private final Elevator.Move move;
+    	private final Elevator.Hold hold;
+    	
+    	public ElevatorManager() {
+    		move = robot.elevator.new Move();
+    		hold = robot.elevator.new Hold();
+    	}
+    	
+    	public void run() {
+    		double leftY = manip.leftStick.y.get();
+    		if( leftY == 0 ) {
+    			hold.activate();
+    		} else {
+    			move.liftPower.set(leftY);
+    			move.activate();
+    		}
+    	}
+    }
+    
     private enum CurrentDrive {
         IDLE, ARCADE, TANK
     }
--- src/com/_604robotics/robot2018/modules/Elevator.java
+++ src/com/_604robotics/robot2018/modules/Elevator.java
@@ -1,69 +1,53 @@
 package com._604robotics.robot2018.modules;
 
-import com._604robotics.robot2018.constants.Calibration;
 import com._604robotics.robot2018.constants.Ports;
+import com._604robotics.robotnik.Action;
+import com._604robotics.robotnik.Input;
 import com._604robotics.robotnik.Module;
-import com._604robotics.robotnik.Output;
-import com._604robotics.robotnik.prefabs.controller.AntiWindupPIDController;
-import com._604robotics.robotnik.prefabs.devices.RateLimitedMotor;
+import com._604robotics.robotnik.prefabs.devices.HoldMotor;
 
-import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator extends Module {
-	private final Encoder encoder = new Encoder(Ports.ELEVATOR_ENCODER_A,
-            Ports.ELEVATOR_ENCODER_B,
-            true,
-            CounterBase.EncodingType.k4X);
 	
-	private final Spark motor = new Spark(Ports.ELEVATOR_MOTOR);
+	public Spark motor = new Spark(Ports.ELEVATOR_MOTOR);
+	public Encoder encoder = new Encoder(Ports.ELEVATOR_ENCODER_A, Ports.ELEVATOR_ENCODER_B);
+	public HoldMotor manualControl = new HoldMotor(motor, encoder);
 	
-	private final RateLimitedMotor rateLimitedMotor = new RateLimitedMotor(encoder, Calibration.ELEVATOR_RATE_TARGET, Calibration.ELEVATOR_RATE_TOLERANCE, motor);
+	public final Action hold = new Hold();
 	
-	private final AntiWindupPIDController pid = 
-			new AntiWindupPIDController(Calibration.ELEVATOR_P, Calibration.ELEVATOR_I, Calibration.ELEVATOR_A, Calibration.ELEVATOR_C, Calibration.ELEVATOR_D, encoder, rateLimitedMotor);
-	
-	public final Output<Integer> elevatorClicks = addOutput("Elevator Clicks", encoder::get);
-	public final Output<Double> elevatorRate = addOutput("Elevator Rate", encoder::getRate);
-	
-	/* Begin jankiest part in this entire robot code */
-	
-	private  Timer timer = new Timer();
-    private boolean timing = false;
-	
-	public final Boolean atElevatorTarget() {
-		if (pid.isEnable() && pid.onTarget()) {
-            if (!timing) {
-                timing = true;
-                timer.start();
-            }
-            
-            return timer.get() >= 0.25;
-        } else {
-            if (timing) {
-                timing = false;
-                
-                timer.stop();
-                timer.reset();
-            }
-            
-            return false;
+	public class Hold extends Action {
+        public Hold () {
+            super(Elevator.this, Hold.class);
         }
+
+        @Override
+        public void run () {
+            manualControl.pidWrite(0);
+        }
+    }
+	
+	public class Move extends Action {
+		public final Input<Double> liftPower;
+		
+		public Move() {
+			this(0);
+		}
+		
+		public Move (double power) {
+			super(Elevator.this, Move.class);
+			liftPower = addInput("Lift Power", power, true);
+		}
+		
+		@Override
+		public void run () {
+			manualControl.pidWrite(liftPower.get());
+		}
 	}
 	
-	public final Output<Boolean> atTarget = addOutput("At Target", this::atElevatorTarget);
-	/* End jankiest part in this entire robot code */
-	
 	public Elevator() {
-		super(Elevator.class);
-
-		SmartDashboard.putData("Elevator PID", pid);
-		pid.setAbsoluteTolerance(12);
-		encoder.setDistancePerPulse(1);
-		encoder.reset();
-		rateLimitedMotor.setRampRate(0.01);
+        super(Elevator.class);
+        setDefaultAction(hold);
 	}
 }
