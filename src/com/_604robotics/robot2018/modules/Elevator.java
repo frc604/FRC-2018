5f987bb273071bb0110fb9dce61b472ce76ce4e5
rlee287 <rlee287@yahoo.com>
2018-01-21 10:18:36 -0800

    Whitespace part 2

--- src/com/_604robotics/robot2018/modules/Elevator.java
+++ src/com/_604robotics/robot2018/modules/Elevator.java
@@ -14,28 +14,28 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator extends Module {
-	private final Encoder encoder = new Encoder(Ports.ELEVATOR_ENCODER_A,
+    private final Encoder encoder = new Encoder(Ports.ELEVATOR_ENCODER_A,
             Ports.ELEVATOR_ENCODER_B,
             true,
             CounterBase.EncodingType.k4X);
-	
-	private final Spark motor = new Spark(Ports.ELEVATOR_MOTOR);
-	
-	private final RateLimitedMotor rateLimitedMotor = new RateLimitedMotor(encoder, Calibration.ELEVATOR_RATE_TARGET, Calibration.ELEVATOR_RATE_TOLERANCE, motor);
-	
-	private final AntiWindupPIDController pid = 
-			new AntiWindupPIDController(Calibration.ELEVATOR_P, Calibration.ELEVATOR_I, Calibration.ELEVATOR_A, Calibration.ELEVATOR_C, Calibration.ELEVATOR_D, encoder, rateLimitedMotor);
-	
-	public final Output<Integer> elevatorClicks = addOutput("Elevator Clicks", encoder::get);
-	public final Output<Double> elevatorRate = addOutput("Elevator Rate", encoder::getRate);
-	
-	/* Begin jankiest part in this entire robot code */
-	
-	private  Timer timer = new Timer();
+    
+    private final Spark motor = new Spark(Ports.ELEVATOR_MOTOR);
+    
+    private final RateLimitedMotor rateLimitedMotor = new RateLimitedMotor(encoder, Calibration.ELEVATOR_RATE_TARGET, Calibration.ELEVATOR_RATE_TOLERANCE, motor);
+    
+    private final AntiWindupPIDController pid = 
+            new AntiWindupPIDController(Calibration.ELEVATOR_P, Calibration.ELEVATOR_I, Calibration.ELEVATOR_A, Calibration.ELEVATOR_C, Calibration.ELEVATOR_D, encoder, rateLimitedMotor);
+    
+    public final Output<Integer> elevatorClicks = addOutput("Elevator Clicks", encoder::get);
+    public final Output<Double> elevatorRate = addOutput("Elevator Rate", encoder::getRate);
+    
+    /* Begin jankiest part in this entire robot code */
+    
+    private  Timer timer = new Timer();
     private boolean timing = false;
-	
-	public final Boolean atElevatorTarget() {
-		if (pid.isEnabled() && pid.onTarget()) {
+    
+    public final Boolean atElevatorTarget() {
+        if (pid.isEnabled() && pid.onTarget()) {
             if (!timing) {
                 timing = true;
                 timer.start();
@@ -52,18 +52,18 @@ public class Elevator extends Module {
             
             return false;
         }
-	}
-	
-	public final Output<Boolean> atTarget = addOutput("At Target", this::atElevatorTarget);
-	/* End jankiest part in this entire robot code */
-	
-	public Elevator() {
-		super(Elevator.class);
+    }
+    
+    public final Output<Boolean> atTarget = addOutput("At Target", this::atElevatorTarget);
+    /* End jankiest part in this entire robot code */
+    
+    public Elevator() {
+        super(Elevator.class);
 
-		SmartDashboard.putData("Elevator PID", pid);
-		pid.setAbsoluteTolerance(12);
-		encoder.setDistancePerPulse(1);
-		encoder.reset();
-		rateLimitedMotor.setRampRate(0.01);
-	}
+        SmartDashboard.putData("Elevator PID", pid);
+        pid.setAbsoluteTolerance(12);
+        encoder.setDistancePerPulse(1);
+        encoder.reset();
+        rateLimitedMotor.setRampRate(0.01);
+    }
 }
