d7b220d356a60f7a70a688e4a968d471ff097d7a
rlee287 <rlee287@yahoo.com>
2018-02-05 14:37:06 -0800

    Make SimultaneousCoordinator abstract like StatefulCoordinator

--- src/com/_604robotics/robotnik/prefabs/coordinators/SimultaneousCoordinator.java
+++ src/com/_604robotics/robotnik/prefabs/coordinators/SimultaneousCoordinator.java
@@ -1,15 +1,23 @@
 package com._604robotics.robotnik.prefabs.coordinators;
 
+import java.util.ArrayList;
+
 import com._604robotics.robotnik.Coordinator;
 import com._604robotics.robotnik.utils.annotations.Untested;
 
 @Deprecated @Untested("Needs to be actually used")
-public class SimultaneousCoordinator extends Coordinator {
+public abstract class SimultaneousCoordinator extends Coordinator {
     
-    private Coordinator coordinators[];
+    private ArrayList<Coordinator> coordinators = new ArrayList<>();
 
     public SimultaneousCoordinator(Coordinator... coords) {
-        coordinators=coords;
+        for (Coordinator co: coords) {
+            addCoordinator(co);
+        }
+    }
+    
+    public void addCoordinator(Coordinator coord) {
+        coordinators.add(coord);
     }
 
     @Override
@@ -22,6 +30,7 @@ public class SimultaneousCoordinator extends Coordinator {
     @Override
     protected boolean run() {
         boolean currentstate=false;
+        // Continue running until all are done
         for (Coordinator co:coordinators) {
             // execute does nothing if the coordinator is in stopped state
             currentstate = currentstate || co.execute();
