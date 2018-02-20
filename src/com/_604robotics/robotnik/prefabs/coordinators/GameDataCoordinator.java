3561bd99bed2ce82c6a0ae9cff56355ab8605a03
2017Driver <2017-Why-Try-To-Email-Me@spilled.java>
2018-02-19 16:08:44 -0800

    Add safety timeout to GameDataCoordinator

--- src/com/_604robotics/robotnik/prefabs/coordinators/GameDataCoordinator.java
+++ src/com/_604robotics/robotnik/prefabs/coordinators/GameDataCoordinator.java
@@ -1,33 +1,41 @@
 package com._604robotics.robotnik.prefabs.coordinators;
 
 import com._604robotics.robotnik.Coordinator;
+import com._604robotics.robotnik.Logger;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 
 public class GameDataCoordinator extends Coordinator {
     private String gameData = "";
+    private Timer timeout = new Timer();
+    private Logger logGameData = new Logger(GameDataCoordinator.class);
     
     @Override
     protected void begin() {
-        // Do nothing
+        timeout.start();
     }
 
     @Override
     protected boolean run() {
         gameData = DriverStation.getInstance().getGameSpecificMessage();
-        return (gameData!=null) && (gameData.length()>0);
+        return (gameData==null) || (gameData.length()==0) || (timeout.get()<1);
     }
 
     @Override
     protected void end() {
-        //Do nothing
+        logGameData.info("Game data is "+gameData);
+        timeout.reset();
     }
 
     public GameDataCoordinator() {
-        // TODO Auto-generated constructor stub
+        // Do nothing
     }
     
     public String getGameData() {
+        if (gameData==null || gameData.length()==0) {
+            logGameData.info("Attempted to get Game Data while it was not ready!");
+        }
         return gameData;
     }
 
