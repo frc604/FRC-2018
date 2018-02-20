e1d60309689c494ebef646898ca4ad632e93c9bf
Michael Smith <michael@spinda.net>
2018-02-20 10:33:17 -0800

    Implement Marionette framework

--- src/com/_604robotics/marionette/InputPlayer.java
+++ src/com/_604robotics/marionette/InputPlayer.java
@@ -1,7 +1,67 @@
 package com._604robotics.marionette;
 
-/**
- * Created by Michael Smith on 2/20/2018.
- */
+import edu.wpi.first.wpilibj.Joystick;
+
 public class InputPlayer {
+    private InputRecording playbackRecording;
+    private double playbackTimestamp;
+    private int playbackFrame;
+
+    public void startPlayback (final InputRecording recording) {
+        playbackRecording = recording;
+        playbackTimestamp = System.currentTimeMillis();
+        playbackFrame = 0;
+    }
+
+    public void stopPlayback () {
+        playbackRecording = null;
+    }
+
+    public boolean isPlaying () {
+        return advanceFrame();
+    }
+
+    public double getPlaybackTime () {
+        return advanceFrame() ? System.currentTimeMillis() - playbackTimestamp : 0;
+    }
+
+    public int getAxisCount (final int joystick, final int defaultValue) {
+        return advanceFrame() ? playbackRecording.getAxisCount(joystick) : defaultValue;
+    }
+
+    public int getButtonCount (final int joystick, final int defaultValue) {
+        return advanceFrame() ? playbackRecording.getButtonCount(joystick) : defaultValue;
+    }
+
+    public double getRawAxis (final int joystick, final int axis, double defaultValue) {
+        if (advanceFrame()) {
+            return playbackRecording.getRawAxis(playbackFrame, joystick, axis);
+        }
+        return defaultValue;
+    }
+
+    public boolean getRawButton (final int joystick, final int button, boolean defaultValue) {
+        if (advanceFrame()) {
+            return playbackRecording.getRawButton(playbackFrame, joystick, button);
+        }
+        return defaultValue;
+    }
+
+    private boolean advanceFrame () {
+        if (playbackRecording == null) {
+            return false;
+        }
+
+        final double elapsedTime = getPlaybackTime();
+        while (playbackFrame < playbackRecording.getFrameCount() && elapsedTime >= playbackRecording.getTimestamp(playbackFrame + 1)) {
+            ++playbackFrame;
+        }
+
+        if (playbackFrame >= playbackRecording.getFrameCount()) {
+            stopPlayback();
+            return false;
+        }
+
+        return true;
+    }
 }
