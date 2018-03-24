39acc2c9f47f1f762e61377fac9d543eb551678f
InfinityPhase <4a.zanella@gmail.com>
2018-03-24 11:42:30 -0700

    Revert "Wrapper for single callable"
    This reverts commit 46853e526d977a7f2c2156a7e9579140bc729257.

--- src/com/_604robotics/robotnik/Task.java
+++ src/com/_604robotics/robotnik/Task.java
@@ -1,44 +1,17 @@
 package com._604robotics.robotnik;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public final class Task <V> implements AutoCloseable { 
+public abstract class Task <V> implements Callable<V>, AutoCloseable { 
+	private V result;
 	private ExecutorService exe;
 	private Future<V> future;
 	
-	public Task( Callable<V> callable ) {
+	public Task() {
 		exe = Executors.newSingleThreadExecutor();
-		future = exe.submit( callable );
 	}
 	
-	public void cancel() {
-		future.cancel( true );
-	}
-	
-	public boolean isReady() {
-		return future.isDone();
-	}
-	
-	public V get() throws InterruptedException, ExecutionException {
-		return future.get();
-	}
-	
-	public V getIfReady() throws InterruptedException, ExecutionException {
-		if( isReady() ) {
-			return get();
-		}
-		
-		return null;
-	}
-	
-	@Override
-    public void close() {
-        if (exe!=null && !exe.isShutdown()) {
-            exe.shutdown();
-        }
-    }
 }
