3392107d6bd049a513bc64299af07a68c436a896
InfinityPhase <4a.zanella@gmail.com>
2018-03-24 10:29:09 -0700

    Working on task wrapper for callables

--- src/com/_604robotics/robotnik/Task.java
+++ src/com/_604robotics/robotnik/Task.java
@@ -2,15 +2,16 @@ package com._604robotics.robotnik;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public abstract class Task <V> implements Callable<V>, AutoCloseable { 
 	private V result;
 	private ExecutorService exe;
-	private Future<v> future;
+	private Future<V> future;
 	
 	public Task() {
-		
+		exe = Executors.newSingleThreadExecutor();
 	}
 	
 }
