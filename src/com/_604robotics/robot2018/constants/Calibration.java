59b9bb41136ac9c5e92b3e0fdc2eb0cf90fffa11
2017Driver <2017-Why-Try-To-Email-Me@spilled.java>
2018-04-19 14:36:19 -0500

    Adjust outtaking power for manipulator

--- src/com/_604robotics/robot2018/constants/Calibration.java
+++ src/com/_604robotics/robot2018/constants/Calibration.java
@@ -138,9 +138,9 @@ public class Calibration {
     /* Intake */
     public static final double INTAKE_INTAKE_MODIFIER = 0.5;
     public static final double INTAKE_OUTAKE_DRIVER_MODIFIER = 0.27;//0.35
-    public static final double INTAKE_OUTAKE_MANIPULATOR_MODIFIER = 0.25;
+    public static final double INTAKE_OUTAKE_MANIPULATOR_MODIFIER = 0.35;
     public static final double INTAKE_OUTAKE_DRIVER_OVERDRIVE_MODIFIER = 0.8; //0.4
-    public static final double INTAKE_OUTAKE_MANIPULATOR_OVERDRIVE_MODIFIER = 0.4;
+    public static final double INTAKE_OUTAKE_MANIPULATOR_OVERDRIVE_MODIFIER = 0.6;
     public static final double INTAKE_PASSIVE_POWER = 0.1;
     
     /* Marionette */
