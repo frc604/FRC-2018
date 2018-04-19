4eb021fa98df7c2a9cab546f0de97bb4b49c6918
2017Driver <2017-Why-Try-To-Email-Me@spilled.java>
2018-04-19 11:57:34 -0500

    Slightly increase outtake for driver

--- src/com/_604robotics/robot2018/constants/Calibration.java
+++ src/com/_604robotics/robot2018/constants/Calibration.java
@@ -137,7 +137,7 @@ public class Calibration {
     
     /* Intake */
     public static final double INTAKE_INTAKE_MODIFIER = 0.5;
-    public static final double INTAKE_OUTAKE_DRIVER_MODIFIER = 0.25;//0.35
+    public static final double INTAKE_OUTAKE_DRIVER_MODIFIER = 0.27;//0.35
     public static final double INTAKE_OUTAKE_MANIPULATOR_MODIFIER = 0.25;
     public static final double INTAKE_OUTAKE_DRIVER_OVERDRIVE_MODIFIER = 0.8; //0.4
     public static final double INTAKE_OUTAKE_MANIPULATOR_OVERDRIVE_MODIFIER = 0.4;
