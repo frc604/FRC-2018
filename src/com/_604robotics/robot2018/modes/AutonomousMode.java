798d8dd6789ed2e6497c6bd7703b8f87836a8d87
rlee287 <rlee287@yahoo.com>
2018-07-23 13:32:45 -0700

    Comments and printing of angle data
    Stick with this for now even though old system uses erroneous encoder properties

--- src/com/_604robotics/robot2018/modes/AutonomousMode.java
+++ src/com/_604robotics/robot2018/modes/AutonomousMode.java
@@ -61,14 +61,17 @@ public class AutonomousMode extends Coordinator {
             class PathFollowTask extends TimerTask{
 
                 private double getCurvature(Trajectory.Segment prev_seg, Trajectory.Segment seg) {
+                    // Get component form of the velocity vectors
                     double prev_vx=prev_seg.velocity*Math.cos(prev_seg.heading);
                     double prev_vy=prev_seg.velocity*Math.sin(prev_seg.heading);
                     double curr_vx=seg.velocity*Math.cos(seg.heading);
                     double curr_vy=seg.velocity*Math.sin(seg.heading);
 
+                    // Deriative of the tangent vector with respect to time
                     double tang_prime_x=(curr_vx-prev_vx)/prev_seg.dt;
                     double tang_prime_y=(curr_vy-prev_vy)/prev_seg.dt;
 
+                    // Normalized normal vector
                     double normal_x=-prev_vy;
                     double normal_y=prev_vx;
 
@@ -77,6 +80,7 @@ public class AutonomousMode extends Coordinator {
                     normal_y/=normal_mag;
 
                     // Dot product of t' dot t_norm
+                    // Minus sign so that positive points right
                     double curvature=-1*(normal_x*tang_prime_x+normal_y*tang_prime_y); 
                     return curvature;
                 }
@@ -99,6 +103,8 @@ public class AutonomousMode extends Coordinator {
                     System.out.print("Left: "+lcurv);
                     System.out.print("|Right: "+rcurv);
                     System.out.println("|Ave: "+curvature);
+                    
+                    double degrees = AutonMovement.clicksToDegrees(Calibration.DRIVE_PROPERTIES, leftEncoderPos-rightEncoderPos);
 
                     double k_kappa=0.1;
                     double k_ptheta=0;
